4181be52a71985d370152555da837dae3a8d53c1
jonatasNs1 <jonatassantos152409@gmail.com>
2024-01-29 10:47:56 -0300

    configuracoes da amazon

---
+++ Compartilhado/Amazon.cs
@@ -0,0 +1,22 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2;
+using Compartilhado.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amazon;
+
+namespace Compartilhado
+{
+    public static class Amazon
+    {
+        public static async Task SalvarAsync(this Pedido pedido)
+        {
+            var client = new AmazonDynamoDBClient(RegionEndpoint.SAEast1);
+            var context = new DynamoDBContext(client);
+            await context.SaveAsync(pedido);
+        }
+    }
+}
